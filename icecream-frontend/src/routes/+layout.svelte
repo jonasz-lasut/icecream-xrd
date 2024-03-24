bb31553eb7aac8b39356c0f0f22b2bf1205ed576
jonasz-lasut <jonasz.lasut@gmail.com>
2024-03-24 14:28:00 +0100

    finished frontend

--- icecream-frontend/src/routes/+layout.svelte
+++ icecream-frontend/src/routes/+layout.svelte
@@ -1,14 +1,5 @@
 <script>
 	import '../app.postcss';
-	import { DarkMode } from 'flowbite-svelte';
-	import { Navbar } from 'flowbite-svelte';
 </script>
-
-<Navbar
-	navDivClass="flex flex-wrap justify-start"
-	navClass="px-2 sm:px-4 py-2.5 bg-gray-300 dark:bg-gray-900"
->
-	<DarkMode btnClass="mr-10" />
-</Navbar>
 
 <slot />
